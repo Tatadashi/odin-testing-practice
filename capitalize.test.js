f15d37d6baf6cbcd7052565d8c7462a19924c2c7
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-01-28 03:31:30 -0800

    Add 2 more tests to capitalize

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,11 @@ import { capitalize } from './capitalize.js'
 test('capitalizes cat to Cat', () => {
     expect(capitalize('cat')).toMatch('Cat');
 });
+
+test("capitalizes c to C", () => {
+  expect(capitalize("c")).toMatch("C");
+});
+
+test("capitalizes At to At", () => {
+  expect(capitalize("At")).toMatch("At");
+});
